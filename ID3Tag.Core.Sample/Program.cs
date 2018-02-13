052347dd7de71a0f5ea5325e16d10165466f15d3
cr1tika7 <kordas.pascal@yahoo.de>
2018-02-13 21:13:30 +0100

    Personal initial commit

---
+++ ID3Tag.Core.Sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using ID3TagReader.ID3v1;
+using System;
+
+namespace ID3TagReader_Sample
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ID3v1Reader reader = new ID3v1Reader();
+            ID3v1Object resp = reader.Read("test.mp3");
+            if (resp != null)
+                Console.WriteLine(resp.Artist);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ID3Tag.Core/ID3v1/ID3v1Reader.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Text;
+
+namespace ID3TagReader.ID3v1
+{
+    public class ID3v1Reader
+    {
+        //ID3 Index and length of fields
+        private const int HEADER_INDEX = 0;
+        private const int HEADER_LENGTH = 3;
+        private const int TITLE_INDEX = 3;
+        private const int TITLE_LENGTH = 30;
+        private const int ARTIST_INDEX = 33;
+        private const int ARTIST_LENGTH = 30;
+        private const int ALBUM_INDEX = 63;
+        private const int ALBUM_LENGTH = 30;
+        private const int YEAR_INDEX = 93;
+        private const int YEAR_LENGTH = 4;
+        private const int COMMENT_INDEX = 97;
+        private const int COMMENT_LENGTH = 4;
+        private const int ZEROBINARY_INDEX = 125;
+        private const int TRACK_INDEX = 126;
+        private const int GENREID_INDEX = 127;
+
+        //ID3+ Index and length of fields
+
+        public ID3v1Object Read(string fileName)
+        {
+            ID3v1Object ret = null;
+            if (!File.Exists(fileName))
+                return ret;
+            using (FileStream fs = new FileStream(fileName, FileMode.Open,FileAccess.Read))
+            {
+                byte[] id3buffer = new byte[128];
+                string id3string = Encoding.ASCII.GetString(id3buffer);
+                if (id3string.Substring(HEADER_INDEX,HEADER_LENGTH) == "TAG")
+                {
+                    ret = new ID3v1Object()
+                    {
+                        Title = id3string.Substring(TITLE_INDEX, TITLE_LENGTH),
+                        Artist = id3string.Substring(ARTIST_INDEX, ARTIST_LENGTH),
+                        Album = id3string.Substring(TITLE_INDEX, TITLE_LENGTH),
+                        Year = id3string.Substring(YEAR_INDEX, YEAR_LENGTH),
+                        Comment = id3string.Substring(COMMENT_INDEX, COMMENT_LENGTH),
+                        GenreID = id3buffer[GENREID_INDEX]
+                    };
+                    if (id3buffer[ZEROBINARY_INDEX] == 0)
+                    {
+                       ret.Track = id3buffer[TRACK_INDEX];
+                    } 
+                }
+            }
+            return ret;
+        }
+
+    }
+}
